ee0479ee890d6660b4c2e8e9e47a8cffc5263f34
Angel Gonzalez <angel.gon.llacer@gmail.com>
2024-09-12 01:22:10 +0100

    adds optional second input in CLI to write the output to a file

--- bin/github-repo-serializer.js
+++ bin/github-repo-serializer.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
 const serializeGitHubRepo = require('../lib/index');
 
+// Get the repository URL and optional output file path from the command-line arguments
 const repoUrl = process.argv[2];
+const outputFile = process.argv[3];
 
 if (!repoUrl) {
   console.error('Please provide a GitHub repository URL');
@@ -10,7 +13,21 @@ if (!repoUrl) {
 }
 
 serializeGitHubRepo(repoUrl)
-  .then(output => console.log(output))
+  .then(output => {
+    if (outputFile) {
+      // Write the output to the specified file
+      fs.writeFile(outputFile, output, (err) => {
+        if (err) {
+          console.error('Error writing to file:', err);
+          process.exit(1);
+        }
+        console.log(`Output written to ${outputFile}`);
+      });
+    } else {
+      // Print the output to the console
+      console.log(output);
+    }
+  })
   .catch(error => {
     console.error('Error:', error);
     process.exit(1);
